5af1e7e7599094f042ac859be7f43db236d43f11
Jagdamba <Jagdamba@JE-46>
2023-08-12 16:25:38 +0530

    commit added roll no

--- src/com/jagdambha/advance/Test.java
+++ src/com/jagdambha/advance/Test.java
@@ -4,16 +4,10 @@ public class Test {
 	
 	public static void main(String[] args) {
 		int pinCode =445001;
-		String cityName= "Yavatmal";
-		// this is the word
-<<<<<<< HEAD
+		int cityName= "Yavatmal";
+	    long mobileno = 767655l;
+	    int rollno = 20;
 		
-		
-		System.out.println("Hello From Computer Dept");
-=======
-		long mobile = 67890564L;
-		System.out.println("Hello From Eclipse");
->>>>>>> branch 'master' of https://github.com/shritambaskar/Jagdambha_College.git
 	}
 
 }
