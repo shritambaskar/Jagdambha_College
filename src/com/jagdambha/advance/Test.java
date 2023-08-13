124ee9433169d5bd4c909bd57311ad1e9da740c8
yashs <yashs@LAPTOP-NQ6U4539>
2023-08-13 19:58:58 +0530

    xyz

--- src/com/jagdambha/advance/Test.java
+++ src/com/jagdambha/advance/Test.java
@@ -1,20 +1,47 @@
 package com.jagdambha.advance;
-
+import java.util.Scanner;
 public class Test {
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.print("Enter total marks: ");
+	        double totalMarks = scanner.nextDouble();
+
+	        System.out.print("Enter marks obtained: ");
+	        double obtainedMarks = scanner.nextDouble();
+
+	        double percentage = (obtainedMarks / totalMarks) * 100;
+
+	        System.out.println("Obtained Marks: " + obtainedMarks);
+	        System.out.println("Total Marks: " + totalMarks);
+	        System.out.println("Percentage: " + percentage + "%");
+
+	        if (percentage >= 80) {
+	            System.out.println("Grade: A");
+	        } else if (percentage >= 60) {
+	            System.out.println("Grade: B");
+	        } else if (percentage >= 40) {
+	            System.out.println("Grade: C");
+	        } else {
+	            System.out.println("Grade: F");
+	        }
+
+	        scanner.close();
+	    }
 	
-	public static void main(String[] args) {
-		int pinCode =445001;
+	//public static void main(String[] args) {
+		//int pinCode =445001;
 <<<<<<< HEAD
-		String cityName= "Yavatmal";
+		//String cityName= "Yavatmal";
 		// this is the word
-		long mobile = 67890564L;
+		//long mobile = 67890564L;
 		//System.out.println("hello jagadamba");
 =======
-		int cityName= "Yavatmal";
-	    long mobileno = 767655l;
-	    int rollno = 20;
+		//int cityName= "Yavatmal";
+	    //long mobileno = 767655l;
+	    //int rollno = 20;
 		
->>>>>>> branch 'master' of https://github.com/shritambaskar/Jagdambha_College.git
+>>>>>>> //branch 'master' of https://github.com/shritambaskar/Jagdambha_College.git
 	string branch="Computer"
 <<<<<<< HEAD
 	//System.out.println("hello jagadamba");
@@ -22,7 +49,7 @@ public class Test {
 >>>>>>> //branch 'master' of https://github.com/shritambaskar/Jagdambha_College.git
 	//https://github.com/shritambaskar/Jagdambha_College.git
 	
-	int count= 64;
+	//int count= 64;
 System.out.println("hello jagadamba college of engineering ytl");
 //to check
 	}
